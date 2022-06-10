b15c9afb3815113cc5d41b0a333e59d68a35d05e
William Kotesky <williamkotesky@gmail.comgit config --listgit config --global user.email williamkotesky@gmail.comgit config --global user.email williamkotesky@gmail.com>
2022-06-10 17:55:14 -0300

    reestruturação da lógica/finalização do cronometro

--- assets/js/cronometro.js
+++ assets/js/cronometro.js
@@ -1,62 +1,54 @@
 class Cronometro {
-    constructor () {
-        this.segundos =0;
-        this.timer;
-        this.cronometroLocal = document.querySelector('.cronometro-display');
-    }
-
     controle() {
-        
-        const cronometro = this.criarDisplay(this.segundos);
-        this.cronometroLocal.innerText = cronometro;
-        let segundosAtt = this.transporteSegundos();
+        let segundos = 0;
+        let timer;
+        const cronometroLocal = document.querySelector('.cronometro-display');
+        cronometroLocal.innerHTML = '00:00:00';
+
+        function criarDisplay(segundos) {
+
+            let tempo = new Date(segundos * 1000);
+            const cronometro = tempo.toLocaleTimeString('pt-BR', {
+                timeStyle: 'medium',
+                timeZone: 'UTC',
+                hour12: false
+            })
+            return cronometro;
+        }
+
+        function iniciar() {
+            timer = setInterval(function () {
+                segundos++
+                cronometroLocal.innerHTML = criarDisplay(segundos);
+            }, 1000)
+        }
 
         document.addEventListener('click', (e) => {
             const element = e.target;
-            
-            if(element.classList.contains('btn-iniciar')) {
-                clearInterval(this.timer);
-                
-                this.iniciar(this.segundos);
-                this.segundos = segundosAtt;
-                console.log(this.segundos)
-                
-            }
 
-            if(element.classList.contains('btn-pausar')) {
-                this.pausar();
-                
-            }
-        })
-    }
-
-    criarDisplay(segundos) {
-        let tempo = new Date(segundos*1000);
-        const cronometro = tempo.toLocaleTimeString('pt-br', {
-            timeStyle: 'medium', 
-            timeZone: 'UTC',
-            hours12: false
-        })
-        return cronometro;
-    }
-
-    iniciar(segundos) {
-        this.timer = setInterval(() => {
-            segundos++
-            this.cronometroLocal.innerHTML = this.criarDisplay(segundos);
-            this.transporteSegundos(segundos)
-
-        }, 1000)
-
+            if (element.classList.contains('btn-reiniciar')) {
+                clearInterval(timer);
+                cronometroLocal.innerHTML = '00:00:00';
+                segundos = 0;
+                cronometroLocal.classList.remove('blink')
         
+            }
+
+            if (element.classList.contains('btn-pausar')) {
+                clearInterval(timer);
+                cronometroLocal.classList.add('blink')
+                
+            }
+
+            if (element.classList.contains('btn-iniciar')) {
+                clearInterval(timer);
+                iniciar();
+                cronometroLocal.classList.remove('blink')
+            }
+
+        })
+
     }
-
-    transporteSegundos(segundos) {segundos}
-
-    pausar() {
-        clearInterval(this.timer);
-    }
-
 
 }
 
